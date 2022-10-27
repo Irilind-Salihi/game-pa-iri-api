b21161be1cf6694d6bbbd2f436fcb7d3fb9c8d2d
Irilind-Salihi <irilind.salihi@gmail.com>
2022-10-27 15:47:20 +0200

    Push Début token

---
+++ config/jwt_config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    SECRET: "bezkoder-secret-key"
+  };
--- server.js
+++ server.js
@@ -1,23 +1,97 @@
-//Minimal express.js index.js file
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-var jsonwebtoken = require('jsonwebtoken');
-var bodyParser = require('body-parser');
+// Chargement des modules
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+//import secret
+const { SECRET } = require('./config/jwt_config')
+//import middleware
+const { extractBearerToken, checkTokenMiddleware } = require('./middleware/token_check')
+const PORT = 1234
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Initialisation de Express 4
+const app = express()
 
-require("./routes/routesAuth")(app);
+app.use(cors())                                 // Activation de CORS
+app.use(express.json())                         // Activation du raw (json)
+app.use(express.urlencoded({ extended: true })) // Activation de x-wwww-form-urlencoded
 
-//import routes
-var routes = require('./routes/routesAuth');
+// Liste des utilisateurs
+const users = [
+    { id: 1, username: 'admin', password: 'password123' }
+]
 
-SECRET = 'mysecret';
-app.get('/', function(req, res) {
-    res.send('Hello World');
-});
+/* Ici les futures routes */
+//route to say hello world
+app.get('/hello', (req, res) => {
+    return res.json({ message: 'Hello World' })
+})
 
-app.listen(port, function() {
-    console.log('Server running on port: ' + port);
-});
+app.get('/me', checkTokenMiddleware, (req, res) => {
+    // Récupération du token
+    const token = req.headers.authorization && extractBearerToken(req.headers.authorization)
+    // Décodage du token
+    const decoded = jwt.decode(token, { complete: false })
+
+    return res.json({ content: decoded })
+})
+
+app.post('/register', (req, res) => {
+    // Aucune information à traiter
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Error. Please enter username and password' })
+    }
+
+    // Checking
+    const userExisting = users.find(u => u.username === req.body.username)
+
+    // Pas bon
+    if (userExisting) {
+        return res.status(400).json({ message: `Error. User ${req.body.username} already existing` })
+    }
+
+    // Données du nouvel utilisateur
+    const id = users[users.length - 1].id + 1
+    const newUser = {
+        id: id,
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    // Insertion dans le tableau des utilisateurs
+    users.push(newUser)
+
+    return res.status(201).json({ message: `User ${id} created` })
+})
+
+app.get('*', (req, res) => {
+    return res.status(404).json({ message: 'Page not found' })
+})
+
+/* Formulaire de connexion */
+app.post('/login', (req, res) => {
+    // Pas d'information à traiter
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Error. Please enter the correct username and password' })
+    }
+
+    // Checking
+    const user = users.find(u => u.username === req.body.username && u.password === req.body.password)
+
+    // Pas bon
+    if (!user) {
+        return res.status(400).json({ message: 'Error. Wrong login or password' })
+    }
+
+    const token = jwt.sign({
+        id: user.id,
+        username: user.username
+    }, SECRET, { expiresIn: '3 hours' })
+
+    return res.json({ access_token: token })
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
